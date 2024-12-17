5fc0886c3b07745fc84ed1b7e4b1794cb3fa5c85
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-12-17 09:36:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,9 +5,9 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 
 # Environment variables
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("apiid"))
+API_HASH = os.getenv("apihash")
+BOT_TOKEN = os.getenv("tk")
 
 # Initialize Pyrogram client
 app = Client("m3u8_downloader_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)

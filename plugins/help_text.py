aed0001a88d3e8ccbc11b819ae0264acc976660b
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-12-17 15:13:58 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -4,6 +4,6 @@ from config import Config
 from globals import AuthU
 from Func.simples import mention_user
 
-@app.on_message(filters.command("start"))
+@Client.on_message(filters.command("start"))
 async def st_rep(client,message:Message):
     await message.reply(f"**🔰RVX M3U8 Downloader🔰\n\n❤️Welcome {mention_user(message)}💪!\n🔰I am an simple M3U8 link uploader bot.🔰Give me a m3u8 link with `/m3u8 <DirectLink>` and \n😇I will upload it to telegram as `video - mp4`🫡😎**")
